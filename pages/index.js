777de1de58b1bbb64e7a8cc96ab5eac45cbb8175
asjadreza <mdasjad.reza@symbtechnologies.com>
2024-06-03 01:33:59 +0530

    added custom hook and center component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Sidebar from "@/components/Sidebar";
+import Center from "@/components/Center";
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main>
+      <main className="flex">
         {/* sidebar */}
         <Sidebar />
         {/* center */}
+        <Center />
       </main>
 
       <div>
